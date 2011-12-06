52f9e89d6e65f6d21ac0086a2a7e22c91ec72a7e
Jeff Schwaber <freyley@gmail.com>
2011-12-06 10:18:32 -0800

    values returned fit with jquery-autocomplete better

--- ajaxwidgets/views.py
+++ ajaxwidgets/views.py
@@ -1,20 +1,22 @@
-from django.http import HttpResponse
 from django import forms
 
 class SearchForm(forms.Form):
-    q = forms.CharField()
+    term = forms.CharField()
 
-from lib.decorators import jsonapi
+from .decorators import jsonapi
 
 @jsonapi
 def search(request, **kwargs):
     model = kwargs["model"]
     search_field = kwargs["search_field"]
+
     form = SearchForm(request.GET)
+
     if form.is_valid():
-        objs = model.objects.filter(**{search_field+'__icontains':form.cleaned_data['q']})
+        objs = model.objects.filter(**{search_field+'__icontains':form.cleaned_data['term']})
     else:
         objs = model.objects.all()[:50]
 
-    vals = objs.values(search_field, 'id')
-    return list(vals)
+    field_data = objs.values_list(search_field, 'id')
+    vals = [{'label' : fd[0], 'id' : fd[1]} for fd in field_data]
+    return vals
