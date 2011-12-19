b7f72ccd980ea06248f8b8ff8ec5041a2d4c7620
Chris Pitzer <chris@lofiart.com>
2011-12-19 14:34:29 -0800

    codeveloping ajaxwidgets with that project

--- ajaxwidgets/fields.py
+++ ajaxwidgets/fields.py
@@ -14,5 +14,21 @@ class MultiModelField(forms.Field):
         super(MultiModelField, self).__init__(*args, **kwargs)
 
     def to_python(self, value):
-        # this is where we turn the ids into model objects
-        import ipdb; ipdb.set_trace()
+        # this is where we turn the string into a list of ids
+        try:
+            ids = [ int(v.strip()) for v in value.split(',')]
+        except ValueError:
+            raise forms.ValidationError("%s not a valid intlist" % value)
+
+    def clean(self, value):
+        ids = self.to_python(value)
+        model_objects = []
+        for id in ids:
+            try:
+                model_objects.append(self.model.objects.get(pk=id))
+            except self.model.objects.DoesNotExist:
+                raise forms.ValidationError("id %d not found" % id)
+        return model_objects
+
+
+
