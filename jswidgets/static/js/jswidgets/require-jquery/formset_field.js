7e19cc51b61bb3e92809052699c7ea0e539d1f92
Chris Pitzer <chris@lofiart.com>
2012-01-17 12:49:16 -0800

    making form deletion work correctly

--- jswidgets/static/js/jswidgets/require-jquery/formset_field.js
+++ jswidgets/static/js/jswidgets/require-jquery/formset_field.js
@@ -6,19 +6,20 @@ define([], function() {
         data.template_text = $(template_tag).html();
         data.name = $(template_tag).data("name");
         data.total_forms = 0;
+        data.forms = [];
 
         $.each($(".jswidgets-formsetfield-form-" + data.name), function(_, form) {
             var f = $(form);
-            f.number = data.total_forms;
             set_up_form_events(f, data);
             data.total_forms += 1;
+            data.forms.push(f);
         });
 
         update_management_form(data);
 
         $("a.jswidgets-formsetfield-addform-" + data.name).click(function() {
             var form = $(data.template_text.replace(/__prefix__/g, data.total_forms));
-            form.number = data.total_forms;
+            data.forms.push(form);
             set_up_form_events(form, data);
             $(this).before(form);
 
@@ -35,21 +36,25 @@ define([], function() {
     };
 
 
-    var renumber_form = function(form, number) {
-        console.log("renumbering", form, number);
+    var renumber_form = function(form, number, data) {
+        console.log("renumbering", form, number, data);
+        debugger;
     };
 
 
     var delete_form = function(form, data) {
         console.log("delete", form, data);
-        console.log(form.number);
-        var id_field = form.find("#id_jswidgets-book_formats-" + form.number + "-id");
+        var index = $.inArray(form, data.forms);
+        var id_field = form.find("#id_jswidgets-book_formats-" + index + "-id");
+
+        debugger;
+
         if(id_field.val() == "") {
             // form was created on this page
             form.remove();
             var total = 0;
             $.each($(".jswidgets-formsetfield-form-" + data.name), function(_, f) {
-                renumber_form(f, total);
+                renumber_form(f, total, data);
                 total += 1;
             });
             data.total_forms = total;
