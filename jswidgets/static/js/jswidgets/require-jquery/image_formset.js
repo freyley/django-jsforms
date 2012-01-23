1ec3d81132d85e1931e3687d56bbfbad33acbfa2
Chris Pitzer <chris@lofiart.com>
2012-01-23 15:12:26 -0800

    image uploading works, still need to modify page post upload

--- jswidgets/static/js/jswidgets/require-jquery/image_formset.js
+++ jswidgets/static/js/jswidgets/require-jquery/image_formset.js
@@ -1,40 +1,41 @@
 define(
-["jqueryui", "./jquery.form"],
+["jqueryui", "./jquery.form", "./resig_micro_templating", "./json2"],
 function() {
 
+    var form_tmpl = '<form id="<%= id %>" action="<%= action %>" style="display:none;" method="POST" enctype="multipart/form-data"><input type="file" name="timage"/></form>';
 
-
-var upload_options = {
-    dataType: "json",
-    type: "post",
-    clearForm: true,
-    success: function(json, x, y, form) {
-        console.log("i done gots stuff from the server", this.arguments);
-
-    }
-};
-
-
-
-
-
-
+    var upload_options = {
+        success: function(text) {
+            // remove textarea tags
+            text = text.substring(10,text.length-11)
+            var data = JSON.parse(text);
+            console.log(data);
+        }
+    };
 
 
     $(function() {
-        var temp_form = $('<form id="jswidgets-image-uploader" style="display:none;" method="POST" enctype="multipart/form-data"><input type="file" name="timage"/></form>');
-        $("body").append(temp_form);
-        var uploader_form = $("#jswidgets-image-uploader");
-        var file_field = uploader_form.find("input");
 
         $("body").on("click", "button.jswidgets-thumbnailimage", function() {
-            var id = $(this).data('hidden_id');
-            uploader_form.find("input").click();
+            var id = $(this).data('hidden_id'),
+                form_id = id + "_form",
+                form_text = tmpl(form_tmpl, {
+                    id: form_id,
+                    action: $(this).data('upload_url')
+                });
 
-            file_field.off("change");
+            $("#" + form_id).remove();
+            $("body").append(form_text);
+
+            form = $("#" + form_id);
+            var file_field = form.find("input");
+
+            form.find("input").click();
+
+            form.ajaxForm(upload_options);
+
             file_field.on("change", function() {
-                console.log("a file has been selected");
-                console.log("upload temp_form now using the ajaxForm");
+                form.submit();
             });
 
             return false;
