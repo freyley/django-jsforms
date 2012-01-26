c7e50132a0777530b2c8ac8224e2e42f919d9757
Chris Pitzer <chris@lofiart.com>
2012-01-25 17:18:22 -0800

    fixed thumbnail regeneration bug.

--- jsforms/models.py
+++ jsforms/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from . import utils
 
+
 class TemporaryUploadedImage(models.Model):
-    timage = models.ImageField(max_length=500, upload_to="temporary_images")
+    timage = models.ImageField(max_length=500, upload_to=utils.upload_to)
 
     def get_thumb_url(self):
         return utils.get_or_create_thumbnail(image=self.timage)
--- jsforms/utils.py
+++ jsforms/utils.py
@@ -3,6 +3,7 @@ from django.core.files.storage import default_storage
 from django.core.files import File
 import os
 from cStringIO import StringIO
+import datetime
 
 JSFORMS_THUMBNAIL_SIZE = getattr(settings, 'JSFORMS_THUMBNAIL_SIZE', (45,45))
 
@@ -16,6 +17,23 @@ except ImportError:
         if settings.JSFORMS_IMAGEUPLOAD_FEATURE:
             raise Exception("jsforms did not find PIL. Either turn off the Image Upload Feature or install PIL.")
     except AttributeError: pass
+
+
+def upload_to(instance, fullpath):
+    dirname, filename = os.path.split(fullpath)
+    if s3:
+        path = os.path.join(
+            'jswidgets/temporary_images',
+            str(int(datetime.datetime.now().strftime("%s")) * 1000),
+            filename
+            )
+    else:
+        # TODO: we should implement this
+        return 'we seem to not have implemented this feature'
+    print "uploading to %s" % path
+    return path
+
+
 
 def fs_thumbnail_generator(image=None, name=None):
     orig_path = os.path.join(settings.MEDIA_ROOT, image.path)
@@ -36,6 +54,7 @@ def s3_thumbnail_generator(image=None, name=None):
     thumb_filename = "jswidgets/thumbs/%s" % image.name
     try:
         aws_file = default_storage.open(thumb_filename)
+        print "I found a thumbnail", thumb_filename
     except IOError:
         thumb = PIL.open(image.file)
         thumb.thumbnail(JSFORMS_THUMBNAIL_SIZE)
@@ -44,12 +63,15 @@ def s3_thumbnail_generator(image=None, name=None):
         filelikeobj.seek(0)
         default_storage.save(thumb_filename, File(filelikeobj))
         aws_file = default_storage.open(thumb_filename)
+        print "I built a thumbnail", thumb_filename
     return default_storage.url(aws_file.name)
 
 def get_or_create_thumbnail(*args, **kwargs): return ''
+s3 = False
 if PIL:
     storage = getattr(settings, 'DEFAULT_FILE_STORAGE', 'fs')
     if 's3' in storage.lower():
+        s3 = True
         get_or_create_thumbnail = s3_thumbnail_generator
     else:
         get_or_create_thumbnail = fs_thumbnail_generator
